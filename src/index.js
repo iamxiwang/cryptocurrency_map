ef7c0b4fcc1d99e719f04e08d793141858cd83f6
XiWang <vivian.xi.wang@gmail.com>
2022-12-04 21:33:48 -0800

    Fetch data &&  did css

--- src/index.js
+++ src/index.js
@@ -14,23 +14,62 @@ window.fetchCountry = fetchCountry;
         function drawRegionsMap() {
             var data = google.visualization.arrayToDataTable(
                 [
-                ['Country', 'Index Score', 'Life Expectancy'],['Germany', 387, 65],
-                ['United States', 653, 75],['Brazil',562, 70],['Canada', 368, 70],['France', 200, 70],['Russia', 541, 70],['China', 535,87],
-                ['India', 663,60],['Nigeria', 521,40],['Vietnam', 1000,67],['Philippines', 753, 66],['Ukrian', 694,66],['Australia',270, 77],
-                ['Egypy',361,65],['Argentina',510,76],['Bolivia', 266,77],['Mexico', 322,88],['Chile', 220,63],['Indonesia', 396,78],
-                ['New Zealand', 129,80],['Moroco',507,78],
-                ['Turkey', 519,66],['United Kindom', 473,66],
-                ['Spain',297,79],['South Africa', 309,56],
-                ['Kenya', 397,45],['Angola', 103,60],
-                ['Japan', 339,90],['South Korea',361,90],['Saudi Arabia',199,89],['Italy',241,80],['Algeria',254,76],['Colombia',496,66],
-                ['Ecuador',409,55],['Peru',287,62]
-
+                ['Country','Life Expectancy(yrs)','Women(yrs)'],
+                ['South Africa', 64.4, 67.9],
+                ['Blugaria ', 71.4, 78.2],
+                ['Russia', 73.2, 78.2],
+                ['Lativia',73.4, 78.2],
+                ['India', 69.6, 71.2],
+                ['Romania',72.9, 76.7],
+                ['Indonesia', 71.9, 74.2],
+                ['Lithuania', 74.5, 79],
+                ['Hungry',74.5, 78],
+                ['Slovak Replic', 74.8, 78.3],
+                ['Poland',75.6, 79.7],
+                ['Estonia',76.9, 81.3],
+                ['Mexico', 75.2, 78.1],
+                ['Brazil', 76.1, 79.7],
+                ['Argentina',76.8, 80.1],
+                ['Columbia', 76.8, 80.0],
+                ['Croatia',76.8, 79.9],
+                ['United States', 77., 79.9],
+                ['Czech Republic',77.4, 80.6],
+                ['Peru',77, 79.7],
+                ['China', 77.1, 79.4],
+                ['Turkey', 78.6, 81.3],
+                ['Greece', 80.3, 83.0],
+                ['Slovenia',80.9,84.0],
+                ['Portugal',81.2, 84.3],
+                ['Costa Rica',80.8,83.3],
+                ['Chile', 81.0, 83.8],
+                ['United Kindom', 80.4, 82.4],
+                ['Germany',80.9,83.5],
+                ['Austria',81.3,83.8],
+                ['Finland', 82.0, 84.7],
+                ['France',82.5, 85.5],
+                ['Belgium',81.9, 84.4],
+                ['Canada', 81.7, 84.0],
+                ['Denmark',81.4, 83.3],
+                ['Netherlands', 81.5, 83.1],
+                ['Spain',83.3,86.2],
+                ['Israel', 82.6, 84.6],
+                ['South Korea',83.5,86.5],
+                ['New Zealand',82.3,84.1],
+                ['Italy',82.9,85.1],
+                ['Luxembourg',52.8, 84.9],
+                ['Ireland',82.6,84.4],
+                ['Australia', 83.2, 85.3],
+                ['Sweden', 83.2, 85.0],
+                ['Japan', 84.7, 87.7],
+                ['Norway',83.2, 84.7],
+                ['Iceland',83.2, 84.5],
+                ['Switzerlan', 84.0, 85.9]
             ]
             );
 
             var options = {
                 title: 'Toppings I Like On My Pizza',
-                colors: ['#f6c7b6','#f3b49f','#ec8f6e','#e6693e','#e0440e' ],
+                colors: ['#f7e3dc','#f5d4c9','#f6c7b6','#ec8f6e','#db3800' ],
                 backgroundColor: {fill: '#CACFD2'}}
 
             var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
@@ -54,12 +93,31 @@ window.fetchCountry = fetchCountry;
                 console.log(countryData)
                 let name = countryData[0].name
                 let fertility = countryData[0].fertility
-                let life_expectancy = countryData[0].life_expectancy_female;
-                let internet_user = countryData[0].internet_user;
-                document.querySelector("#name").innerHTML = name;
-                document.querySelector("#fertility").innerHTML = "fertility:  " + fertility;
-                document.querySelector("#life").innerHTML = "life_expectancy_female:  " + life_expectancy;
-                document.querySelector("#internet_user").innerHTML = "internet_user: " + internet_user;
+                let life_expectancy = countryData[0].life_expectancy_male;
+                let internet_user = countryData[0].internet_users;
+                let exports =  countryData[0].exports;
+                let gdp_growth =  countryData[0].gdp_growth;
+                let gdp_per_capita =  countryData[0].gdp_per_capita;
+                let homicide_rate =  countryData[0].homicide_rate ;
+                let imports =  countryData[0].improts;
+                let infant_mortality =  countryData[0].infant_mortality;
+
+                document.querySelector("#countryname").innerHTML = name;
+                document.querySelector("#fertility").innerHTML = "Fertility:   " + fertility +'(birth/woman)';
+                document.querySelector("#life").innerHTML = "Life Expectancy Male: " + life_expectancy +'years';
+                document.querySelector("#internet_user").innerHTML = "Internet_Users:   " + internet_user +'%';
+
+                document.querySelector("#export").innerHTML = "exports:" + exports;
+
+                document.querySelector("#gdp_growth").innerHTML = "Gdp Growth:" + gdp_growth;
+
+                document.querySelector("#gdp_per_capita").innerHTML ="Gdp Per Capita:" + gdp_per_capita;
+
+                document.querySelector("#homecide_rate").innerHTML ="Homecide Rate:" + homicide_rate + "%";
+
+                document.querySelector("#import").innerHTML ="Import: "+ imports;
+
+                document.querySelector("#infant_mortality").innerHTML = "Infant Mortality:" + infant_mortality;
 
             }
             
